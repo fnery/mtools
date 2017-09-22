be905c952afd90d8886a3d0427b66b0837396f5a
fnery <fabiornery@gmail.com>
2017-09-22 11:11:35 +0100

    now 'fileparts2.m' works with cells making 'filepartscell.m' obsolete

--- misc/fileparts2.m
+++ misc/fileparts2.m
@@ -1,20 +1,25 @@
 function [pathStr, name, ext] = fileparts2(in)
-% fileparts2.m: extends fileparts.m to work in files with 2 exts (e.g. .nii.gz)
+% fileparts2.m: extends fileparts.m
 %
 % Syntax:
 %    1) [pathStr, name, ext] = fileparts2(in)
 %
 % Description:
-%    1) [pathStr, name, ext] = fileparts2(in) extends fileparts.m to work in
-%        files with 2 exts (e.g. .nii.gz)
+%    1) [pathStr, name, ext] = fileparts2(in) extends fileparts.m providing
+%        two additional features:
+%        - works in files with 2 extensions (e.g. .nii.gz)
+%        - accepts a cell of strings as input
 %
 % Inputs:
 %    1) in: string corresponding to the complete file path or name
+%           OR
+%           cell of strings with multiple file paths
 %
 % Outputs:
 %    1) pathStr: directory where the file lives
 %    2) name   : file name, excluding extension
 %    3) ext    : file extension
+%    (strings or cells depending on input)
 %
 % Notes/Assumptions: 
 %    1) Created specifically for ".nii.gz" files
@@ -33,6 +38,18 @@ function [pathStr, name, ext] = fileparts2(in)
 %    []
 %
 % fnery, 20170922: original version
+
+if iscell(in)
+    [pathStr, name, ext] = cellfun(@filepartswrap, in, 'UniformOutput', 0);
+elseif ischar(in)
+    [pathStr, name, ext] = filepartswrap(in);
+else
+    error('Error: input must be a string or a cell of strings');
+end
+
+end
+
+function [pathStr, name, ext] = filepartswrap(in)
 
 [pathStr , name  , ext ] = fileparts(in);
 [      ~ , name2 , ext2] = fileparts(name);
