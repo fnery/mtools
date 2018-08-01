aea0fe527794c4db84b4977daa8564d23c8c2540
fnery <fabiornery@gmail.com>
2018-08-01 16:57:24 +0100

    copyfiles.m/movefiles.m now also accept destination directory as input

--- misc/copyfiles.m
+++ misc/copyfiles.m
@@ -1,4 +1,4 @@
-function copyfiles(old, new)
+function out = copyfiles(old, new)
 % copyfiles.m: copies multiple files (MATLAB's copyfile wrapper)
 %
 % Syntax:
@@ -10,19 +10,24 @@ function copyfiles(old, new)
 %
 % Inputs:
 %    1) old: cell of strings (source file paths)
-%    2) new: cell of strings (destination file paths)
+%    2) new: can be two things:
+%       i)  cell of strings (destination file paths)
+%       ii) string (char) (fullpath to directory to which files will be copied)
 %
 % Outputs:
-%    []
+%    1) out: cell of strings (full paths of files after copying)
 %
 % Notes/Assumptions: 
-%    []
+%    1) If 'new' is a directory (char) the file names will be the same
+%       after copying, whereas if providing 'new' as a cell of strings the
+%       file names after copying can be different from the source files
 %
 % References:
 %    []
 %
 % Required functions:
 %    1) exist2.m
+%    2) fileparts2.m
 %
 % Required files:
 %    []
@@ -31,6 +36,23 @@ function copyfiles(old, new)
 %    []
 %
 % fnery, 20180801: original version
+%                  now also accepts destination directory as input
+
+if ischar(new)
+    % assume:
+    % 1) 'new' is a directory    
+    % 2) we want to copy the files in 'old' into it
+    % therefore I need to generate the destination paths to all the files
+    
+    % Check 'new' directory exists
+    exist2(new, 'dir', true);
+    
+    % Generate destination paths
+    [~, n, e] = fileparts2(old);
+    new = fullfile(new, strcat(n, e));
+    
+    % Now proceed as if the user had provided a cell of strings in 'new'
+end   
 
 % Error checks
 nOld = length(old);
@@ -40,7 +62,8 @@ if ~iscell(old)
     error('Error: ''old'' must be a cell of strings (source paths of files to be copied)');
 end
 if ~iscell(new)
-    error('Error: ''new'' must be a cell of strings (destination paths of files to be created)');
+    error(['Error: ''new'' must be either 1) a cell of strings (destination paths of files to be copied)', ...
+     '\n                            2) a string (full path to directory to which files will be copied)'], []);
 end
 
 if ~isequal(nOld, nNew)
@@ -73,5 +96,7 @@ for iPath = 1:nPaths
     end
 
 end
+
+out = new;
 
 end
--- misc/movefiles.m
+++ misc/movefiles.m
@@ -1,4 +1,4 @@
-function movefiles(old, new)
+function out = movefiles(old, new)
 % movefiles.m: moves multiple files (MATLAB's movefile wrapper)
 %
 % Syntax:
@@ -10,19 +10,24 @@ function movefiles(old, new)
 %
 % Inputs:
 %    1) old: cell of strings (source file paths)
-%    2) new: cell of strings (destination file paths)
+%    2) new: can be two things:
+%       i)  cell of strings (destination file paths)
+%       ii) string (char) (fullpath to directory to which files will be moved)
 %
 % Outputs:
-%    []
+%    1) out: cell of strings (full paths of files after moving)
 %
 % Notes/Assumptions: 
-%    []
+%    1) If 'new' is a directory (char) the file names will be the same
+%       after moving, whereas if providing 'new' as a cell of strings the
+%       file names after moving can be different from the source files
 %
 % References:
 %    []
 %
 % Required functions:
 %    1) exist2.m
+%    2) fileparts2.m
 %
 % Required files:
 %    []
@@ -31,6 +36,23 @@ function movefiles(old, new)
 %    []
 %
 % fnery, 20180801: original version
+%                  now also accepts destination directory as input
+
+if ischar(new)
+    % assume:
+    % 1) 'new' is a directory    
+    % 2) we want to move the files in 'old' into it
+    % therefore I need to generate the destination paths to all the files
+    
+    % Check 'new' directory exists
+    exist2(new, 'dir', true);
+    
+    % Generate destination paths
+    [~, n, e] = fileparts2(old);
+    new = fullfile(new, strcat(n, e));
+    
+    % Now proceed as if the user had provided a cell of strings in 'new'
+end   
 
 % Error checks
 nOld = length(old);
@@ -39,8 +61,9 @@ nNew = length(new);
 if ~iscell(old)
     error('Error: ''old'' must be a cell of strings (source paths of files to be moved)');
 end
-if ~iscell(new)
-    error('Error: ''new'' must be a cell of strings (destination paths of files to be moved)');
+if ~iscell(new) 
+    error(['Error: ''new'' must be either 1) a cell of strings (destination paths of files to be moved)', ...
+     '\n                            2) a string (full path to directory to which files will be moved)'], []);
 end
 
 if ~isequal(nOld, nNew)
@@ -73,5 +96,7 @@ for iPath = 1:nPaths
     end
 
 end
+
+out = new;
 
 end
