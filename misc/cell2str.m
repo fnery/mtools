7ff85b6574208505e5d198ae3cde783e6005f897
fnery <fabiornery@gmail.com>
2018-08-12 21:18:10 +0100

    cell2str.m: now accepts both row and column cell vectors

--- misc/cell2str.m
+++ misc/cell2str.m
@@ -1,64 +1,62 @@
 function out = cell2str(in)
-% cell2str.m: converts [n x 1] cell of strings to a [n x nChars] string
-%   
+% cell2str.m: converts "1D" cell of strings to [n x nChars] string
+%
 % Syntax:
 %    1) out = cell2str(in)
 %
 % Description:
-%    1) out = cell2str(in) converts a [n x 1] cell of strings to a 
-%      [n x nChars] string
+%    1) out = cell2str(in) converts "1D" cell of strings to [n x nChars]
+%       string
 %
 % Inputs:
-%    1) in: [n x 1] cell of strings
+%    1) in: "1D" cell of strings
 %
 % Outputs:
-%    1) out: [n x nChars] string
+%    1) out: [n x nChars] string, where 'n' is length(in)
 %
-% Notes/Assumptions: 
+% Notes/Assumptions:
 %    1) Right pads each line so that in the end all lines have a length
 %       equal to the longest line.
 %    2) Created originally to help mimic the output of spm_select.m
-%    3) Assumption 1: ''in'' must have only one column
+%    3) Assumption 1: ''in'' must have only one column OR one line (i.e. 
+%       must be a "1D" vector (see is1d.m)
 %
 % References:
 %    []
 %
 % Required functions:
-%    1) strpad.m
+%    1) is1d.m
+%    2) strpad.m
 %
 % Required files:
 %    []
-% 
+%
 % Examples:
 %    in{1,1} = 'ababab';
 %    in{2,1} = 'wow';
 %    in{3,1} = 'longlinetoshowthespacepadding';
 %    out = cell2str(in)
-%    >> ababab                       
+%    >> ababab
 %       wow                            <-- note space-padding in lines 1,2
 %       longlinetoshowthespacepadding
-% 
+%
 % fnery, 20170309: original version
+% fnery, 20180812: now accepts both row and column cell vectors
 
-if ~iscell(in)
-    error('Error: ''in'' must be a cell');
+if ~iscell(in) || ~is1d(in)
+    error('''in'' must be a "1D" cell of strings');
 end
 
-% Assumption 1
-if size(in, 2) > 1
-   error('Error: ''in'' must have only one column');
-end 
-
-nLines = size(in, 1);
+nStrings = length(in);
 
 % We'll have to pad the full files to the length of the longest fullpath
 maxLen = max(cellfun('length',in));
 
 out = '';
-for iLine = 1:nLines
-    cLine = in{iLine}; % Assumption 1
-    cLine = strpad(cLine, maxLen);
-    out(iLine,:) = cLine;
+for iString = 1:nStrings
+    cString = in{iString}; % Assumption 1
+    cString = strpad(cString, maxLen);
+    out(iString,:) = cString;
 end
-    
+
 end
