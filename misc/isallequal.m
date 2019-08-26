1fd895d5b9c9f5df53a19877d697a60d68db800a
fnery <fabiornery@gmail.com>
2019-08-26 13:32:17 +0100

    now accepts cell inputs

--- misc/isallequal.m
+++ misc/isallequal.m
@@ -1,6 +1,6 @@
 function [out] = isallequal(in)
-% isallequal.m: checks if all elements in the array are equal
-%   
+% isallequal.m: checks if all elements in input array are equal
+%
 % Syntax:
 %    1) [out] = isallequal(in)
 %
@@ -9,23 +9,22 @@ function [out] = isallequal(in)
 %       'in' are equal
 %
 % Inputs:
-%    1) in (numeric/char): array
+%    1) in (numeric/char/cell): array
 %
 % Outputs:
-%    1) out (logical): 
+%    1) out (logical):
 %        - 1 (true)  : all elements of 'in' are equal
 %        - 0 (false) : otherwise
 %
-% Notes/Assumptions: 
-%    1) I could make this work with cells but haven't had the time to do so
-%       just yet
+% Notes/Assumptions:
+%    []
 %
 % Required functions:
 %    []
 %
 % Required files:
 %    []
-% 
+%
 % Examples:
 %    in = ones(2,2)
 %    isallequal(in)
@@ -43,14 +42,63 @@ function [out] = isallequal(in)
 %    >> ans =
 %    >>      0
 %
+%    xEqual{1}    = [0 2 3];
+%    xEqual{2}    = [0 2 3];
+%    xEqual{3}    = [0 2 3];
+%    xNotEqual{1} = [0 2 3];
+%    xNotEqual{2} = [0 2 3];
+%    xNotEqual{3} = [0 3 3]; % second element here different
+%    xEqualTF = isallequal(xEqual)
+%    xNotEqualTF = isallequal(xNotEqual)
+%    >> xEqualTF = 1
+%    >> xNotEqualTF = 0
+%
+%    xEqual{1}    = [0 2 3; 9 7 8];
+%    xEqual{2}    = [0 2 3; 9 7 8];
+%    xEqual{3}    = [0 2 3; 9 7 8];
+%    xNotEqual{1} = [0 2 3; 9 7 8];
+%    xNotEqual{2} = [0 2 3; 9 7 8];
+%    xNotEqual{3} = [0 3 3; 9 7 8]; % second element here different
+%    xEqualTF = isallequal(xEqual)
+%    xNotEqualTF = isallequal(xNotEqual)
+%    >> xEqualTF = 1
+%    >> xNotEqualTF = 0
+%
+%    xEqual{1}    = 'asd';
+%    xEqual{2}    = 'asd';
+%    xEqual{3}    = 'asd';
+%    xNotEqual{1} = 'asd';
+%    xNotEqual{2} = 'asd';
+%    xNotEqual{3} = 'axd'; % second element here different
+%    xEqualTF = isallequal(xEqual)
+%    xNotEqualTF = isallequal(xNotEqual)
+%    >> xEqualTF = 1
+%    >> xNotEqualTF = 0
+%
+%    xEqual{1}    = {'asd', 'asd'};
+%    xEqual{2}    = {'asd', 'asd'};
+%    xEqual{3}    = {'asd', 'asd'};
+%    xNotEqual{1} = {'asd', 'asd'};
+%    xNotEqual{2} = {'asd', 'asd'};
+%    xNotEqual{3} = {'axd', 'asd'}; % second element here different
+%    xEqualTF = isallequal(xEqual)
+%    xNotEqualTF = isallequal(xNotEqual)
+%    >> xEqualTF = 1
+%    >> xNotEqualTF = 0
+%
 % fnery, 20150820: original version
 % fnery, 20170325: added valid input checks
+% fnery, 20190826: now accepts cell inputs
 
-if iscell(in) || isstruct(in)
-   error('Error: ''in'' must be a numeric or char array'); 
+if ischar(in) || isstring(in) || isnumeric(in)
+    in  = in(:);
+    out = all(in == in(1));
+elseif iscell(in)
+    warning('isallequal.m invoked with cell inputs, which still requires some validation, so check everything OK')
+    elementwiseCheck = cellfun(@isequal, in, repmat(in(1), size(in)));
+    out = all(elementwiseCheck);
+else
+    error('Error: ''in'' must be a numeric, char, string or cell array');
 end
-
-in  = in(:);
-out = all(in == in(1));
 
 end
